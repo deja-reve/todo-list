ad1fe04d2b1c4699f78594086a29a552cff798d7
alice <alice.lucia.ott@icloud.com>
2024-02-16 07:06:56 -0800

    initial commit...

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Code Challenge TODO App</p>
+        {/* Build your TODO MVC here */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
